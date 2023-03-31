fba65dd44ec875dbe1b849ee89f959c64a411abf
Anatolii Ivaniuk <climatcontrol@ukr.net>
2023-03-31 12:28:59 +0300

    added blog page

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ function browsersync() {
 }
 
 function styles() {
-  return src("app/scss/style.scss")
+  return  src("app/scss/style.scss")
     .pipe(scss({ outputStyle: "compressed" }))
     .pipe(concat("style.min.css"))
     .pipe(
